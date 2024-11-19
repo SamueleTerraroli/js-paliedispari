334fb15edfaa6892129dd446af07c5a1a4cf0f29
SamueleTerraroli <terraroli99@gmail.com>
2024-11-19 14:35:23 +0100

    build: add base file with workflow

---
+++ script.js
@@ -0,0 +1,8 @@
+//Palindroma
+/*
+-creare un prompt per inserire la parola
+-creare una funzione che scompone la parola in un array
+-creare un ciclo for con ordini di ciclo inversi
+-mettere a confronto i 2 array generati 
+-se gli array corrispondono generare un messaggio: la parola è palindroma, viceversa se la parola non è palindroma
+*/
