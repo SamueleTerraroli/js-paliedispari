91349eb5c455f3d86dea7970c2f2c36851e3f1c7
SamueleTerraroli <terraroli99@gmail.com>
2024-11-19 18:36:24 +0100

    fix: reverseArr function

--- script.js
+++ script.js
@@ -7,29 +7,12 @@
 -se gli array corrispondono generare un messaggio: la parola è palindroma, viceversa se la parola non è palindroma
 */
 
-/*let word = prompt('inserisci una parola');
-let arr = word.split("");
-console.log(arr);
 
-newArr=[]
-for (let i = arr.length -1; i >= 0; i--) {
-    newArr.push(arr[i]);
-    newArr.join();
-    if (arr[i] === newArr[i]) {
-        message = `La parola ${word} è palindroma`
-        
-    } else{
-        message = `La parola ${word} NON è palindroma`
-    }
-    
-}
-console.log(newArr);
-console.log(message);
-*/
 let word = prompt('inserisci una parola');
 
 let arr = getArray(word);
-console.log(arr);
+//console.log(arr);
+
 
 let message = reverseArr(arr);
 console.log(message);
@@ -43,11 +26,11 @@ function getArray(parola){
 
 function reverseArr (arrResult){
     newArr = [];
-    
+    let message=""
     for (let i = arrResult.length -1; i >= 0; i--) {
         newArr.push(arrResult[i]);
         newArr.join();
-        message=""
+        
         if (arrResult[i] === newArr[i]) {
             message = `La parola ${word} è palindroma`;
             
@@ -57,3 +40,10 @@ function reverseArr (arrResult){
     }
     return message;
 }
+
+/*function wordUpperCase (parolaMaiuscola){
+    const arrUpper = parolaMaiuscola.substr.toUpperCase();
+
+    return arrUpper;
+}
+*/
